9a3be1701cbb79868040429c05d57b034efc2f52
Jordan Smellie <jsmellie@gmail.com>
2014-07-02 15:16:05 -0400

    Created WinConsole app
    Created blank Windows C# Console application.

---
+++ Quiz - July 7 2014/Quiz - July 7 2014/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz___July_7_2014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
